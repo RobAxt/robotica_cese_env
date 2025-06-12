358b14b746ed5566eb44ff6b95d819b49a6d2d02
RobAxt <roberto.axt@gmail.com>
2025-06-12 10:49:47 -0300

    se logro agregar la mesa y el cubo. Tambien se subio el robot estandar a la mesa

--- src/third-party/mycobot-ros2/mycobot_bringup/scripts/mycobot_gazebo_and_moveit.sh
+++ src/third-party/mycobot-ros2/mycobot_bringup/scripts/mycobot_gazebo_and_moveit.sh
@@ -23,7 +23,7 @@ ros2 launch mycobot_gazebo mycobot.gazebo.launch.py \
     robot_model:=$ROBOT_MODEL \
     x:=0.0 \
     y:=0.0 \
-    z:=0.05 \
+    z:=1.05 \
     roll:=0.0 \
     pitch:=0.0 \
     yaw:=0.0 &
